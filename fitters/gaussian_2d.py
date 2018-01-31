6b41d53bca0df33db3e20c7d4e6e37e04c36ba78
Mark Brown <o.mark.brown@gmail.com>
2018-01-31 10:28:01 -0700

    add function for splitting data into redundant time-series for stability monitoring, added aberration-analysis notebook

--- fitters/gaussian_2d.py
+++ fitters/gaussian_2d.py
@@ -30,7 +30,7 @@ def f_raw(coordinates, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
     a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
     b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
     c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
-    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) + c*((y-yo)**2)))
+    g = offset + amplitude*np.exp(- (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) + c*((y-yo)**2)))
     return g.ravel()
 
 
