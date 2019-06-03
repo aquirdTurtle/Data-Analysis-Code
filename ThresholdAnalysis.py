e0b1a36f3b0aad63c36d8c99f1ce53ab008ff406
Mark Brown <o.mark.brown@gmail.com>
2019-06-02 19:57:25 -0600

    some updates around creating talk, modifications to threshold analysis getting this to work at least in demo

--- ThresholdAnalysis.py
+++ ThresholdAnalysis.py
@@ -2,9 +2,12 @@ import MainAnalysis as ma
 import matplotlib.pyplot as plt
 import Miscellaneous as misc
 import numpy as np
+import ExpFile as exp
+
 
 def updateThresholds(fid, atomLocations, picsPerRep):
-    res = ma.standardPopulationAnalysis(fid, atomLocations, 0, picsPerRep)
+    exp.setPath('17', 'May', '2019')
+    res = standardPopulationAnalysis(fid, atomLocations, 0, picsPerRep)
     (locCounts, thresholds, avgPic, key, allPopsErr, allPops, avgPop, avgPopErr, fits,
      fitModules, keyName, atomData, rawData, atomLocations, avgFits, atomImages,
      totalAvg, totalErr) = res
@@ -13,11 +16,17 @@ def updateThresholds(fid, atomLocations, picsPerRep):
     for i, atomLoc in enumerate(atomLocations):
         ax.hist(locCounts[i], 50, color=colors[i], orientation='vertical', alpha=0.3, histtype='stepfilled')
         ax.axvline(thresholds[i].t, color=colors[i], alpha=0.3)    
+    plt.show()
     # output thresholds
     threshVals = [t.t for t in thresholds]
-    threshVals = np.flip(np.reshape(threshVals, (10,10)),1)
-    with open('J:/Code-Files/T-File.txt','w') as f:
+    threshVals = np.flip(np.reshape(threshVals, (5,5)),1)
+    with open('C:/Users/Mark-Brown/Code/Chimera-Control/T_File.txt','w') as f:
         for row in threshVals:
             for thresh in row:
                 f.write(str(thresh) + ' ') 
     plt.show(block=False)
+
+updateThresholds(38, [2,2,2,5,5], 2)
+
+#plt.plot([0,1,2],[2,1,2],'o:')
+#plt.show()
